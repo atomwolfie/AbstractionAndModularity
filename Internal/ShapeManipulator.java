de1753cfd548b368c06452709ceecd5234a19ad8
Aaron Adams <aaronadams19@gmail.com>
2018-09-10 09:43:47 -0600

    Internal

---
+++ Internal/ShapeManipulator.java
@@ -0,0 +1,21 @@
+package Internal;
+
+import examples.shapes.Circle;
+import examples.shapes.Shape;
+
+public  class ShapeManipulator{
+	
+	
+	
+	public void scale(double scaleFactor){
+		//to do????				
+	};
+	
+	public void move(Shape shape) {
+	
+	//to do????
+	};
+	
+	
+	
+}
